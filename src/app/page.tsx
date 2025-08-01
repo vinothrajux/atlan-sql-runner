a1bf54ae03fee1dc5dac1dff92f8721c6129f4e3
Vinothraj Elumalai <vinothraj.elumalai@freshworks.com>
2025-08-01 12:06:04 +0530

    Checking deployment pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
-            Update new change // checking the deployment pipeline
+            Update new change // checking the deployment pipeline 
           </li>
         </ol>
 
