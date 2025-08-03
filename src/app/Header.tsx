cf380048bcfa2e9dfd306168b01a165b1ccc9a24
Vinothraj Elumalai <vinothrajux@gmail.com>
2025-08-03 13:25:21 +0530

    Theme fix in header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,12 +4,12 @@ import { useTheme } from '../context/ThemeProvider';
 import RadixThemeSwitch from "../components/atoms/RadixThemeSwitch";
 
 export default function Header() {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
-    <>
-    <h1 className="text-2xl font-bold mb-4">ATLAN - SQL Query Runner</h1>
+    <div className={`flex items-center justify-between p-4 bg-${theme === 'dark' ? 'gray-800' : 'white'} text-${theme === 'dark' ? 'white' : 'black'}`}>
+      <h1 className="text-2xl font-bold mb-4">ATLAN - SQL Query Runner</h1>
       <RadixThemeSwitch />
-    </>
+    </div>
   )
 }
