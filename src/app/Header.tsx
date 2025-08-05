25de987bd9700ca659a60e528fb5e92bb139cb06
Vinothraj Elumalai <vinothrajux@gmail.com>
2025-08-05 15:30:16 +0530

    Add next image for optimized image loading

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from '../context/ThemeProvider';
 import RadixThemeSwitch from "../components/atoms/RadixThemeSwitch";
+import Image from 'next/image';
 
 export default function Header() {
   const { theme } = useTheme();
@@ -10,9 +11,9 @@ export default function Header() {
     <div className={`flex items-center sticky justify-between p-4  shadow-lg bg-${theme === 'dark' ? 'gray-800' : 'white'} text-${theme === 'dark' ? 'white' : 'black'}`}>
       <a href="https://atlan.com/" aria-label="Atlan Logo">
       {theme === 'dark' ? 
-        <img width="80" height="24" alt="Atlan Logo" src="https://website-assets.atlan.com/img/logo.svg"  />
+        <Image width={80} height={24} alt="Atlan Logo" src="https://website-assets.atlan.com/img/logo.svg"  />
         :
-        <img width="80" height="24" alt="Atlan Logo" src="https://website-assets.atlan.com/img/atlan-blue.svg" />
+        <Image width={80} height={24} alt="Atlan Logo" src="https://website-assets.atlan.com/img/atlan-blue.svg" />
       }
       </a>
       <h1 className="text-sm sm:text-2xl font-bold text-center px-4">SQL Query Runner</h1>
